ec9449b155205ba7bde3599e26669de2e00c4002
Wisp Jeyne darry <wjeyne-d@e1r10p3.21-school.ru>
2019-01-16 21:27:32 +0300

    norminette lazha

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,19 @@
 /*   By: wjeyne-d <wjeyne-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/23 18:56:18 by wjeyne-d          #+#    #+#             */
-/*   Updated: 2019/01/09 20:55:11 by wjeyne-d         ###   ########.fr       */
+/*   Updated: 2019/01/16 21:26:41 by wjeyne-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-static int ft_is_next_line(char **stack, char **line)
+static int	ft_is_next_line(char **stack, char **line)
 {
-	printf("stroka v kotoroi ischem perenos: %s\n", *stack);
 	char *ptr;
 	char *tmp;
 
+	printf("stroka v kotoroi ischem perenos: %s\n", *stack);
 	if ((ptr = ft_strchr(*stack, '\n')))
 	{
 		printf("PTR ISNEXTLINE: %s\n", ptr);
@@ -30,7 +30,7 @@ static int ft_is_next_line(char **stack, char **line)
 		*stack = ft_strdup(tmp + 1);
 		return (1);
 	}
-	printf("strchr didnt found perenos stroki\n" );
+	printf("strchr didnt found perenos stroki\n");
 	return (0);
 }
 
@@ -41,10 +41,9 @@ int	get_next_line(const int fd, char **line)
 	char *tmp;
 	static char *stack[MAX_FD];
 
-	if ((fd < 0 || line == NULL || read(fd, stack[fd], 0) < 0) || \
-	!(buf = ft_strnew(sizeof(char) * BUFF_SIZE)))
+	if ((fd < 0 || fd > MAX_FD|| line == NULL || read(fd, stack[fd], 0) < 0)
+	\|| !(buf = ft_strnew(sizeof(char) * BUFF_SIZE)))
 		return (-1);
-	//printf("GNL BUF: %s\n", buf);
 	if(stack[fd])
 		if(ft_is_next_line(&stack[fd], line))
 			return(1);
@@ -53,20 +52,13 @@ int	get_next_line(const int fd, char **line)
 		buf[r] ='\0';
 		if(stack[fd])
 		{
-			printf("read bytes: %d\n", r);
 			tmp = stack[fd];
 			stack[fd] = ft_strjoin(tmp, buf);
 			free(tmp);
 			tmp = NULL;
-			printf("GNL STACK: %s\n", stack[fd]);
 		}
 		else
-		{
 			stack[fd] = ft_strdup(buf);
-			//printf("GNL STACK[FD]: %s\n", stack[fd]);
-
-				//ft_is_next_line(&stack[fd], line);
-		}
 		if (ft_is_next_line(&stack[fd], line))
 				break ;
 	}
