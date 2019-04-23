590d7ecf431f26daf342592e0bc0e55e482998b5
wjeyne-d <wjeyne-d@student.21-school.ru>
2019-04-23 16:12:28 +0300

    updated from intra

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wjeyne-d <wjeyne-d@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/23 18:53:22 by wjeyne-d          #+#    #+#             */
+/*   Updated: 2019/01/23 19:17:47 by wjeyne-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 100
+# define MAX_FD 4864
+
+int get_next_line(const int fd, char **line);
+
+#endif
