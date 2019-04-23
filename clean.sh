064f5374281f05c4f7c5c8f45946ef601ff65913
wjeyne-d <wjeyne-d@student.21-school.ru>
2019-04-23 16:11:11 +0300

    old version files removed

--- clean.sh
+++
@@ -1,6 +0,0 @@
-make clean
-rm -f *.o
-rm -f test_gnl
-cd libft
-make fclean
-cd ..
--- gnl_test/main.c
+++ gnl_test/main.c
@@ -1,34 +1,24 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include "get_next_line.h"
-// #include <time.h>
-//
-// clock_t begin = clock();
+
 int main(int argc, char **argv)
 {
 	int g;
 	int fd;
 	char *line;
-//	int i;
 
 	if (argc)
 	{
 		g = 1;
-	//	i = 0;
 
 		fd = open(argv[1], O_RDONLY);
 		while (g > 0)
 		{
-			//i++;
 			g = get_next_line(fd, &line);
-			printf("LINE:%s", line);
-			printf("\n!\n");
+			printf("%s\n", line);
 			ft_strdel(&line);
-			//printf("%d\n", g);
 		}
 		close(fd);
-// 		clock_t end = clock();
-// double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-// printf("%f\n", time_spent);
 	}
 }
